2afa250562dd7d516215fc56a9658ac8adb89d1b
JaeBeen95 <nin2348@gmail.com>
2024-04-15 20:09:15 +0900

    refactor: width가 줄어들 때 글자가 생략되도록 수정

--- src/components/DashBoard/SideMenu/DashboardListSection/DashboardList/index.tsx
+++ src/components/DashBoard/SideMenu/DashboardListSection/DashboardList/index.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 import S from './DashboardList.module.scss'
 import { CROWN } from '../../constants'
 
-const DashboardList = () => {
+interface DashboardListProps {
+  title: string
+  isSelected: boolean
+}
+
+const DashboardList: React.FC<DashboardListProps> = ({ title, isSelected }) => {
+  const [selected, setSelected] = useState(isSelected)
+
   return (
     <div className={S.wrapper}>
       <div className={S['dashboard-color']} />
-      <div className={S['dashboard-title']}>비브리지</div>
+      <div className={S['dashboard-title']}>{title}</div>
       <Image
         className={S.crown}
         src={CROWN}
--- src/components/DashBoard/SideMenu/DashboardListSection/index.tsx
+++ src/components/DashBoard/SideMenu/DashboardListSection/index.tsx
@@ -2,15 +2,27 @@ import DashboardList from './DashboardList'
 import S from './DashboardListSection.module.scss'
 import ListHeader from './ListHeader/ListHeader'
 
+interface MockData {
+  title: string
+  isSelected: boolean
+}
+
+const mockData: MockData[] = [
+  { title: '비브리지', isSelected: false },
+  { title: '코드잇', isSelected: false },
+  { title: '3분기 계획', isSelected: false },
+  { title: '회의록', isSelected: false },
+  { title: '중요 문서함', isSelected: false },
+]
+
 const DashboardListSection = () => {
   return (
     <div className={S.container}>
       <ListHeader />
       <div className={S['dashboard-wrapper']}>
-        <DashboardList />
-        <DashboardList />
-        <DashboardList />
-        <DashboardList />
+        {mockData.map((data, idx) => (
+          <DashboardList key={idx} {...data} />
+        ))}
       </div>
     </div>
   )
