18bf03431b1dea4f281591206881d4bf68b40d1b
yeona <aya813@naver.com>
2024-04-15 20:39:25 +0900

    feat: SettingsContent 컴포넌트 구현

---
+++ src/components/landing/settings-content/constants.tsx
@@ -0,0 +1,23 @@
+export const SECTIONS = [
+  {
+    imgSrc: '/images/landingImg3.png',
+    title: '대시보드 설정',
+    description: '대시보드 사진과 이름을 변경할 수 있어요.',
+    width: 300,
+    height: 123,
+  },
+  {
+    imgSrc: '/images/landingImg4.png',
+    title: '초대',
+    description: '새로운 팀원을 초대할 수 있어요.',
+    width: 300,
+    height: 230,
+  },
+  {
+    imgSrc: '/images/landingImg5.png',
+    title: '구성원',
+    description: '구성원을 초대하고 내보낼 수 있어요.',
+    width: 300,
+    height: 195,
+  },
+]
