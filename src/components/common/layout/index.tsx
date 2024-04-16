db89871ee418198b3716b05f2b5093bb24914f68
choiwonseokgit <cs0328@hanmail.net>
2024-04-17 01:09:58 +0900

    feat: dashboard layout 에 dashboardheader 적용 및 css 수정

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,19 +1,27 @@
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 import SideMenu from '@/src/components/dashboard/side-menu'
 
 import S from './Layout.module.scss'
+import DashboardHeader from '../../dashboard/header'
 
 type LayoutProps = {
   children: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const {
+    query: { id },
+  } = useRouter()
+
   return (
     <div className={S.container}>
       <SideMenu />
-      <div>
-        <nav>nav</nav>
+      <div className={S.rightSideContainer}>
+        <nav>
+          <DashboardHeader id={id} />
+        </nav>
         <main>{children}</main>
       </div>
     </div>
