ebf411058f75a8300a2481b36f517dfde6de032d
yeona <aya813@naver.com>
2024-04-16 14:16:43 +0900

    feat: Profile 컴포넌트 UI

---
+++ src/components/mypage/profile/index.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+
+import S from './Profile.module.scss'
+
+import ADD_IMG from '/public/icons/add-img.svg'
+
+const Profile = () => {
+  return (
+    <div className={S.container}>
+      <h1 className={S.title}>프로필</h1>
+      <div className={S.items}>
+        <div className={S['img-container']}>
+          <Image src={ADD_IMG} alt="이미지 추가하기" width={30} height={30} />
+        </div>
+        <div className={S['item-container']}>
+          <div className={S.item}>
+            <p className={S.text}>이메일</p>
+            <input
+              type="text"
+              className={S[`email-input`]}
+              disabled
+              placeholder="이메일 값 받아오면 추가하기"
+            />
+          </div>
+          <div className={S.item}>
+            <p className={S.text}>닉네임</p>
+            <input type="text" className={S[`name-input`]} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
