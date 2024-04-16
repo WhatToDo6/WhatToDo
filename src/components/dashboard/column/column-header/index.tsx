3e2aacc7b6145ec69c9bbcee1ed1309ed4881437
JaeBeen95 <nin2348@gmail.com>
2024-04-17 08:39:49 +0900

    feat: ColumnHeader 완성

--- src/components/dashboard/column/column-header/index.tsx
+++ src/components/dashboard/column/column-header/index.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image'
+
 import S from './ColumnHeader.module.scss'
+import { SETTING } from '../constants'
 
 const ColumnHeader = () => {
   return (
     <header className={S.wrapper}>
-      <div className={S.ellipse} />
-      <div>To Do</div>
-      <div>3</div>
-      <div>톱니바퀴</div>
+      <div className={S.columnHeading}>
+        <div className={S.ellipse} />
+        <div className={S.columnTitle}>To do</div>
+        <div className={S.cardCount}>3</div>
+      </div>
+      <Image
+        className={S.setting}
+        src={SETTING}
+        width={24}
+        height={24}
+        alt="설정"
+      />
     </header>
   )
 }
