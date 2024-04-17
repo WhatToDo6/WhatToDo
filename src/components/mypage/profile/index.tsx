90a7997ef72c53cb4920b5ccfe73d13788a12203
yeona <aya813@naver.com>
2024-04-17 11:55:43 +0900

    feat: 이미지 업로드 기능 구현

--- src/components/mypage/profile/index.tsx
+++ src/components/mypage/profile/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useRef, useState } from 'react'
 import Image from 'next/image'
 
 import S from './Profile.module.scss'
@@ -7,12 +8,43 @@ import Input from '../input'
 import ADD_IMG from '/public/icons/add-img.svg'
 
 const Profile = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null)
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0]
+      setSelectedImage(URL.createObjectURL(img))
+    }
+  }
+
+  const handleImageUpload = () => {
+    fileInputRef.current?.click()
+  }
+
   return (
     <div className={S.container}>
       <h1 className={S.title}>프로필</h1>
       <div className={S.items}>
-        <div className={S['img-container']}>
-          <Image src={ADD_IMG} alt="이미지 추가하기" width={30} height={30} />
+        <div className={S['img-container']} onClick={handleImageUpload}>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            style={{ display: 'none' }}
+          />
+          {selectedImage ? (
+            <Image
+              src={selectedImage}
+              alt="선택된 이미지"
+              fill
+              unoptimized={true}
+              className={S['preview-image']}
+            />
+          ) : (
+            <Image src={ADD_IMG} alt="이미지 추가하기" width={30} height={30} />
+          )}
         </div>
         <form className={S.form}>
           <Input
