3fea15b4893ec11555eadd05c97db9219eb48e7c
innerstella <suji0509@naver.com>
2024-04-17 15:11:15 +0900

    remove: test code 제거

--- src/components/login-signup/form/login/index.tsx
+++ src/components/login-signup/form/login/index.tsx
@@ -1,25 +1,8 @@
-import { useEffect } from 'react'
-
-import AXIOS from '@/lib/axios'
-
 import Button from '../../button'
 import LogInInput from '../../input/login'
 import S from '../Form.module.scss'
 
 const LogInForm = () => {
-  useEffect(() => {
-    AXIOS.post('/auth/login', {
-      email: 'test@whattodo.com',
-      password: '123456789',
-    })
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
-
   return (
     <form className={S.container}>
       <label className={S.label}>이메일</label>
