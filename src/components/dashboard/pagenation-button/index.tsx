a41e972694cff6c2b3658f209309bf7e3354f1d0
choiwonseokgit <cs0328@hanmail.net>
2024-04-17 21:50:10 +0900

    feat: 페이지네이션 버튼 생성

---
+++ src/components/dashboard/pagenation-button/index.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+import arrowLeft from '@/public/images/small-arrow-left.png'
+import arrowRight from '@/public/images/small-arrow-right.png'
+
+import S from './PagenationButton.module.scss'
+
+interface PagenationButtonProps {
+  currPage: number
+  lastPage: number
+  onClickLeft: () => void
+  onClickRight: () => void
+}
+
+function PagenationButton({
+  currPage,
+  lastPage,
+  onClickLeft,
+  onClickRight,
+}: PagenationButtonProps) {
+  return (
+    <div className={S.container}>
+      <p>
+        {currPage} 페이지 중 {lastPage}
+      </p>
+      <div className={S.btnContainer}>
+        <button onClick={onClickLeft}>
+          <Image src={arrowLeft} alt="이전" />
+        </button>
+        <button onClick={onClickRight}>
+          <Image src={arrowRight} alt="다음" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default PagenationButton
