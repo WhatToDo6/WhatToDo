e87d12d226f486d393fef050d9373cb7d786d6e0
choiwonseokgit <cs0328@hanmail.net>
2024-04-17 17:56:50 +0900

    feat: dashboardbutton 컴포넌트 공용으로 쓸 수있게 수정

--- src/components/dashboard/dashboard-button/index.tsx
+++ src/components/dashboard/dashboard-button/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 import addBoardBtn from '@/public/icons/add-board-btn.svg'
+import crownIcon from '@/public/icons/crown-icon.svg'
 import rightArrow from '@/public/icons/right-arrow.svg'
 
 import S from './dashboardButton.module.scss'
@@ -9,38 +10,53 @@ type MockData = {
   [key: string]: string
 }
 
+type DashboardButtonType =
+  | 'add'
+  | 'addColumn'
+  | 'addDashboard'
+  | 'moveDashboard'
+  | 'deleteDashboard'
+
 interface DashboardButtonProps {
+  type: DashboardButtonType
   dashboard?: MockData
+  onClick?: () => void
 }
 
-function DashboardButton({ dashboard }: DashboardButtonProps) {
-  const btnClassName = `${S.btn} ${
-    dashboard ? S.withDashboard : S.withoutDashboard
-  }`
-
-  if (!dashboard) {
-    return (
-      <button className={btnClassName}>
+function DashboardButton({ dashboard, type, onClick }: DashboardButtonProps) {
+  const BTN_CHILDREN: Record<DashboardButtonType, JSX.Element> = {
+    add: (
+      <Image className={S.addBoardBtn} src={addBoardBtn} alt="addBoardBtn" />
+    ),
+    addColumn: (
+      <>
+        <div>새로운 칼럼 추가하기</div>
+        <Image className={S.addBoardBtn} src={addBoardBtn} alt="addBoardBtn" />
+      </>
+    ),
+    addDashboard: (
+      <>
         <div>새로운 대시보드</div>
-        <Image
-          width={22}
-          height={22}
-          className={S.addBoardBtn}
-          src={addBoardBtn}
-          alt="addBoardBtn"
-        />
-      </button>
-    )
+        <Image className={S.addBoardBtn} src={addBoardBtn} alt="addBoardBtn" />
+      </>
+    ),
+    moveDashboard: (
+      <>
+        <div className={S.infoBox}>
+          <span>img1</span>
+          <div>{dashboard && dashboard.name}</div>
+          <Image src={crownIcon} alt="왕관" />
+        </div>
+        <Image src={rightArrow} alt="rightArrow" />
+      </>
+    ),
+
+    deleteDashboard: <div>대시보드 삭제하기</div>,
   }
 
   return (
-    <button className={btnClassName}>
-      <div className={S.infoBox}>
-        <span>img1</span>
-        <div>{dashboard.name}</div>
-        <span>img1</span>
-      </div>
-      <Image width={18} height={18} src={rightArrow} alt="rightArrow" />
+    <button className={`${S.btn} ${S[type]}`} onClick={onClick}>
+      {BTN_CHILDREN[type]}
     </button>
   )
 }
