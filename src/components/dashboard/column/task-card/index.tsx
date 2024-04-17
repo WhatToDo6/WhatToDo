3671ff5e64192e09dfd30e17515dc72137fe05c3
JaeBeen95 <nin2348@gmail.com>
2024-04-17 19:10:53 +0900

    feat: TaskCardTag 생성

--- src/components/dashboard/column/task-card/index.tsx
+++ src/components/dashboard/column/task-card/index.tsx
@@ -1,9 +1,13 @@
 import S from './TaskCard.module.scss'
+import TaskCardTag from '../task-card-tag'
+
+//TODO TaskCardTag props. 향후 데이터로 받아 와야 함
 
 const TaskCard = () => {
   return (
     <div className={S.container}>
       <div className={S.cardHeading}>새로운 일정 관리 Taskify</div>
+      <TaskCardTag tagType="상" />
     </div>
   )
 }
