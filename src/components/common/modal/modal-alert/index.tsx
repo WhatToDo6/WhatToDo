2755b28858f2cb635b59f5e16f17995f5b8dcbe3
innerstella <suji0509@naver.com>
2024-04-19 00:44:04 +0900

    feat: 버튼 클릭 시 페이지 이동 로직 추가

--- src/components/common/modal/modal-alert/index.tsx
+++ src/components/common/modal/modal-alert/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { useContext } from 'react'
 
 import S from './ModalAlert.module.scss'
@@ -7,20 +8,29 @@ import BorderButton from '../../button/border'
 interface ModalAlertProps {
   content: string
   buttonText: string
+  moveTo?: string
 }
 
-const ModalAlert = ({ content, buttonText }: ModalAlertProps) => {
+/**
+ *
+ * @param content - 모달 내부 내용
+ * @param buttonText - 버튼 텍스트
+ * @param moveTo - (optional) 버튼 클릭 시 이동할 페이지 경로
+ * @returns
+ */
+const ModalAlert = ({ content, buttonText, moveTo }: ModalAlertProps) => {
+  const router = useRouter()
   const modalStaus = useContext(ModalContext)
+  const handleClick = () => {
+    modalStaus.setIsOpen.bind(null, false)
+    moveTo && router.push(moveTo)
+  }
 
   return (
     <div className={S.container}>
       <span className={S.content}>{content}</span>
       <div className={S.button}>
-        <BorderButton
-          color="purple"
-          size="large"
-          onClick={modalStaus.setIsOpen.bind(null, false)}
-        >
+        <BorderButton color="purple" size="large" onClick={handleClick}>
           {buttonText}
         </BorderButton>
       </div>
