7462bdb508861bca296cfe13cde0fa52eb0b6738
JaeBeen95 <nin2348@gmail.com>
2024-04-18 17:37:26 +0900

    feat: 재업로드

--- pages/dashboard/[id]/index.tsx
+++ pages/dashboard/[id]/index.tsx
@@ -1,7 +1,23 @@
 import Layout from '@/src/components/common/layout'
+import Column from '@/src/components/dashboard/column'
+import AddColumn from '@/src/components/dashboard/column/add-column'
+import ColumnLayout from '@/src/components/dashboard/column/column-layout'
+
+import S from './DashboardId.module.scss'
 
 const DashboardIdPage = () => {
-  return <Layout>main</Layout>
+  return (
+    <Layout>
+      <ColumnLayout>
+        <Column />
+        <Column />
+        <Column />
+        <div className={S.addWrapper}>
+          <AddColumn />
+        </div>
+      </ColumnLayout>
+    </Layout>
+  )
 }
 
 export default DashboardIdPage
