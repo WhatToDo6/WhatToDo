c92812ccf7189d8db3f0a4ccb3d01022c25c7b70
choiwonseokgit <cs0328@hanmail.net>
2024-04-19 01:56:55 +0900

    feat: InvitedCard 컴포넌트 분기 처리

--- src/components/dashboard/invited-list/invite-card/index.tsx
+++ src/components/dashboard/invited-list/invite-card/index.tsx
@@ -1,26 +1,58 @@
+import BorderButton from '@/src/components/common/button/border'
 import OptionButton from '@/src/components/common/button/option'
 
 import S from './InviteCard.module.scss'
 
-interface Props {
+type InvitedListType = 'dashboard' | 'member' | 'email'
+
+interface InvitedCardProps {
   name: string
   person: string
+  type: InvitedListType
 }
 
-function InvitedCard({ name, person }: Props) {
-  return (
-    <div className={S.container}>
-      <div>{name}</div>
-      <div>{person}</div>
-      <OptionButton
-        size="medium"
-        leftColor="purple"
-        rightColor="white"
-        leftText="수락"
-        rightText="거절"
-      />
-    </div>
-  )
+function isTypeMemberOrEmail(
+  type: InvitedListType,
+): type is 'member' | 'email' {
+  return type === 'member' || type === 'email'
+}
+
+function InvitedCard({ name, person, type }: InvitedCardProps) {
+  const className = `${S.container} ${isTypeMemberOrEmail(type) ? S.inEdit : ''}`
+
+  const INVITED_CARD = {
+    dashboard: (
+      <>
+        <div>{name}</div>
+        <div>{person}</div>
+        <OptionButton
+          size="medium"
+          leftColor="purple"
+          rightColor="white"
+          leftText="수락"
+          rightText="거절"
+        />
+      </>
+    ),
+    member: (
+      <>
+        <div>{name}</div>
+        <BorderButton size="small" color="white">
+          삭제
+        </BorderButton>
+      </>
+    ),
+    email: (
+      <>
+        <div>{name}</div>
+        <BorderButton size="small" color="white">
+          취소
+        </BorderButton>
+      </>
+    ),
+  }
+
+  return <div className={className}>{INVITED_CARD[type]}</div>
 }
 
 export default InvitedCard
