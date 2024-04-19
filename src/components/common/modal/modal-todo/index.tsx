46c399de15ea4e0a6c6a1cf47d0a2e837996c920
innerstella <suji0509@naver.com>
2024-04-20 00:25:54 +0900

    feat: modal-todo 레이아웃 및 제목 input

---
+++ src/components/common/modal/modal-todo/index.tsx
@@ -0,0 +1,61 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import { InputFormValues } from '@/src/types/input'
+
+import S from './ModalTodo.module.scss'
+import OptionButton from '../../button/option'
+import Input from '../../input'
+
+const ModalTodo = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<InputFormValues>({ mode: 'onBlur' })
+
+  const onSubmit: SubmitHandler<InputFormValues> = (data) => {
+    // TODO: 할 일 생성 로직
+    console.log(data)
+  }
+
+  return (
+    <div className={S.container}>
+      <h1 className={S.title}>할 일 생성</h1>
+      <label className={S.label} htmlFor="keeper">
+        담당자
+      </label>
+      <label className={S.label} htmlFor="title">
+        제목<span className={S.required}>*</span>
+      </label>
+      <Input
+        inputType="title"
+        placeholder="제목을 입력해주세요"
+        error={errors.title}
+        register={register}
+      />
+      <label className={S.label} htmlFor="description">
+        설명<span className={S.required}>*</span>
+      </label>
+      <label className={S.label} htmlFor="due">
+        마감일
+      </label>
+      <label className={S.label} htmlFor="tag">
+        태그
+      </label>
+      <label className={S.label} htmlFor="image">
+        이미지
+      </label>
+      <OptionButton
+        size="large"
+        leftColor="white"
+        rightColor="purple"
+        leftText="취소"
+        rightText="생성"
+        onRightClick={handleSubmit(onSubmit)}
+      />
+    </div>
+  )
+}
+
+export default ModalTodo
