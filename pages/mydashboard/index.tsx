92d05fcfa2d6c196f48d2140524a1c2e76335ae8
choiwonseokgit <cs0328@hanmail.net>
2024-04-20 22:36:46 +0900

    feat: usePagenation myDashboard 페이지 적용

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -1,38 +1,27 @@
 import Layout from '@/src/components/common/layout'
 import DashboardButtonContainer from '@/src/components/dashboard/button-container'
-import InvitedList from '@/src/components/dashboard/invited-list'
-import { usePagenationTest } from '@/src/hooks/usePagenationTest'
+import InviteListDashboard from '@/src/components/dashboard/invited-list/dashboard'
+import { usePagenation } from '@/src/hooks/usePagenation'
+import { DashboardType } from '@/src/types/mydashboard'
 
 import S from './MyDashboard.module.scss'
 
-const MOCK_DATA = [
-  { name: '프로덕트 디자인', person: '손동희' },
-  { name: '유닛A', person: '안귀영' },
-  { name: '유닛B', person: '장혁' },
-  { name: '유닛C', person: '강나무' },
-  { name: '유닛D', person: '안귀영' },
-  { name: '유닛E', person: '장혁' },
-  { name: '유닛F', person: '강나무' },
-  { name: '유닛G', person: '강나무' },
-  { name: '유닛H', person: '강나무' },
-]
-
 const MyDashboard = () => {
-  const { dashboards, currPage, lastPage, onClickPrevPage, onClickNextPage } =
-    usePagenationTest(5)
+  const { pageData, currPage, lastPage, onClickPrevPage, onClickNextPage } =
+    usePagenation<DashboardType>(5, 'dashboard')
 
   return (
     <Layout>
       <div className={S.container}>
         <DashboardButtonContainer
-          dashboards={dashboards}
+          dashboards={pageData}
           currPage={currPage}
           lastPage={lastPage}
           onClickPrevPage={onClickPrevPage}
           onClickNextPage={onClickNextPage}
         />
         <div className={S.invitedDashboard}>
-          <InvitedList inviteData={MOCK_DATA} type="dashboard" />
+          <InviteListDashboard type="dashboard" />
         </div>
       </div>
     </Layout>
