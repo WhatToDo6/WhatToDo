10d130739d53642a31708b566c1bb952e0621a83
yeona <aya813@naver.com>
2024-04-22 17:13:03 +0900

    feat: InputProfileImage 컴포넌트 생성

---
+++ src/components/common/input/profile-image/index.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import { ChangeEvent } from 'react'
+import { useForm } from 'react-hook-form'
+
+import ADD_IMG from '@/public/icons/add-img.svg'
+import EDIT_IMG from '@/public/icons/edit-img.svg'
+import { InputFormValues } from '@/src/types/input'
+
+import S from './ProfileImage.module.scss'
+
+interface InputProfileImageProps {
+  handleImageChange: (e: ChangeEvent<HTMLInputElement>) => void
+  profileImageUrl: string
+}
+
+const InputProfileImage = ({
+  profileImageUrl,
+  handleImageChange,
+}: InputProfileImageProps) => {
+  const { register } = useForm<InputFormValues>()
+
+  return profileImageUrl ? (
+    <div
+      className={S.editContainer}
+      style={{ backgroundImage: `url(${profileImageUrl})` }}
+    >
+      <label htmlFor="profileEdit" className={S.icon}>
+        <Image src={EDIT_IMG} alt="수정" fill />
+      </label>
+      <input
+        id="profileEdit"
+        type="file"
+        {...register('profileImageUrl')}
+        onChange={handleImageChange}
+        className={S.input}
+      />
+    </div>
+  ) : (
+    <div className={S.addContainer}>
+      <label htmlFor="profileAdd" className={S.icon}>
+        <Image src={ADD_IMG} alt="추가" fill />
+      </label>
+      <input
+        id="profileAdd"
+        type="file"
+        {...register('profileImageUrl')}
+        onChange={handleImageChange}
+        className={S.input}
+      />
+    </div>
+  )
+}
+
+export default InputProfileImage
