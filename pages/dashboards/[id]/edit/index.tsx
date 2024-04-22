cb605eda44653213346d6d3f53e0e3d0b1a3a4b9
choiwonseokgit <cs0328@hanmail.net>
2024-04-23 02:55:53 +0900

    feat: 대시보드 수정 페이지 컴포넌트 배치

--- pages/dashboards/[id]/edit/index.tsx
+++ pages/dashboards/[id]/edit/index.tsx
@@ -1,11 +1,68 @@
-import Layout from '@/src/components/common/layout'
-import DashboardEditor from '@/src/components/dashboard/editor'
+import { GetStaticPropsContext } from 'next'
 
-const DashboardIdEdit = () => {
+import { useRouter } from 'next/router'
+
+import AXIOS from '@/lib/axios'
+import BackButton from '@/src/components/common/back-button/Backbutton'
+import Layout from '@/src/components/common/layout'
+import DashboardButton from '@/src/components/dashboard/dashboard-button'
+import DashboardEditor from '@/src/components/dashboard/editor'
+import InviteListEmail from '@/src/components/dashboard/invited-list/email'
+import InviteListMember from '@/src/components/dashboard/invited-list/member'
+
+import S from './DashboardIdEdit.module.scss'
+
+export async function getServerSideProps(context: GetStaticPropsContext) {
+  const id = context.params && context.params['id']
+
+  if (!id)
+    return {
+      notFound: true,
+    }
+
+  return {
+    props: {
+      id,
+    },
+  }
+}
+
+interface DashboardIdEditProps {
+  id: number
+}
+
+const DashboardIdEdit = ({ id }: DashboardIdEditProps) => {
+  const router = useRouter()
+
+  const deleteDashboard = async () => {
+    const token = localStorage.getItem('accessToken')
+    try {
+      AXIOS.delete(`/dashboards/${id}`, {
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const handleButtonCLick = () => {
+    deleteDashboard()
+    router.push('/mydashboard')
+  }
+
   return (
     <Layout>
-      <div />
-      <DashboardEditor />
+      <div className={S.container}>
+        <BackButton />
+        <DashboardEditor dashboardId={id} />
+        <InviteListEmail dashboardId={id} />
+        <InviteListMember dashboardId={id} />
+        <div className={S.buttonBox}>
+          <DashboardButton type="deleteDashboard" onClick={handleButtonCLick} />
+        </div>
+      </div>
     </Layout>
   )
 }
