2b07b7243aaae77bc7a380b5ed6ca7ef885f9f99
yeona <aya813@naver.com>
2024-04-23 01:17:20 +0900

    feat: Comment 컴포넌트 생성

---
+++ src/components/common/modal/modal-task/comment/index.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+import S from './Comment.module.scss'
+
+interface CommentProps {
+  content: string
+  createdAt: string
+  author: {
+    profileImageUrl: string
+    nickname: string
+  }
+}
+
+const Comment = ({ content, createdAt, author }: CommentProps) => {
+  const handleEdit = () => {
+    //TODO 댓글 수정 기능을 구현해야 합니다.
+  }
+
+  const handleDelete = () => {
+    //TODO 댓글 삭제 기능을 구현해야 합니다.
+  }
+
+  return (
+    <div className={S.container}>
+      <Image
+        src={author.profileImageUrl}
+        alt="댓글 프로필"
+        width={34}
+        height={34}
+        className={S.img}
+      />
+      <div className={S.comment}>
+        <div className={S.title}>
+          <span className={S.people}>{author.nickname}</span>
+          <p className={S.createdAt}>{createdAt}</p>
+        </div>
+        <p className={S.description}>{content}</p>
+        <div className={S.option}>
+          <p className={S.optionText} onClick={handleEdit}>
+            수정
+          </p>
+          <p className={S.optionText} onClick={handleDelete}>
+            삭제
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Comment
