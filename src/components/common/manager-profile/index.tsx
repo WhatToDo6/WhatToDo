2b6522fffc000441fa47168bdc3b4ed4e2953d03
yeona <aya813@naver.com>
2024-04-24 15:27:51 +0900

    feat: 계정관리 페이지로 연결

--- src/components/common/manager-profile/index.tsx
+++ src/components/common/manager-profile/index.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 import basicImg from '@/public/icons/temp-circle-1.svg'
 
@@ -8,6 +10,7 @@ interface ManagerProfileProps {
   type: 'dropdown' | 'dashboardHeader' | 'card'
   profileImageUrl: string | null
   nickname: string
+  showPopover?: boolean
 }
 
 /**
@@ -15,6 +18,7 @@ interface ManagerProfileProps {
  * @param type - 'dropdown' | 'dashboardHeader' | 'card'
  * @param profileImageUrl - string | null
  * @param nickname - string
+ * @param showPopover - (optional) 팝오버의 유무를 결정
  * @returns
  */
 
@@ -22,12 +26,30 @@ function ManagerProfile({
   profileImageUrl,
   nickname,
   type,
+  showPopover = false,
 }: ManagerProfileProps) {
   const SIZE = {
     dashboardHeader: 38,
     dropdown: 26,
     card: 34,
   }
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+  const router = useRouter()
+
+  const togglePopover = () => {
+    setIsPopoverOpen((prev) => !prev)
+  }
+
+  const handleMyInfoClick = () => {
+    router.push('/mypage')
+    setIsPopoverOpen((prev) => !prev)
+  }
+
+  const handleLogout = () => {
+    //TODO 로그아웃 구현
+    setIsPopoverOpen((prev) => !prev)
+  }
+
   return (
     <div className={`${S.container} ${S[type]}`}>
       <Image
@@ -36,7 +58,18 @@ function ManagerProfile({
         src={profileImageUrl ? profileImageUrl : basicImg}
         alt="profileImg"
         className={S.img}
+        onClick={togglePopover}
       />
+      {showPopover && isPopoverOpen && (
+        <div className={S.popoverContainer}>
+          <button className={S.popoverOption} onClick={handleMyInfoClick}>
+            내 정보
+          </button>
+          <button className={S.popoverOption} onClick={handleLogout}>
+            로그아웃
+          </button>
+        </div>
+      )}
       <span>{nickname}</span>
     </div>
   )
