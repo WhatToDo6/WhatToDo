6ec2cc93af7b4abbc53b957391af55f6e8bdc964
choiwonseokgit <cs0328@hanmail.net>
2024-04-25 01:40:04 +0900

    feat: layout AppProvider로 한번에 묶어서 context 정리

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { ReactNode } from 'react'
+import { ReactNode, createElement, ComponentType } from 'react'
 
 import SideMenu from '@/src/components/dashboard/side-menu'
 import DashboardsProvider from '@/src/context/dashboards'
@@ -14,27 +14,41 @@ type LayoutProps = {
   children: ReactNode
 }
 
+type ContextType = ComponentType<{ children: ReactNode }>
+
+interface AppProviderProps {
+  contexts: ContextType[]
+  children: ReactNode
+}
+
+const AppProvider: React.FC<AppProviderProps> = ({ contexts, children }) =>
+  contexts.reduce(
+    (prev, context) => createElement(context, null, prev),
+    children,
+  )
+
 const Layout = ({ children }: LayoutProps) => {
   const { pathname } = useRouter()
 
   return (
-    <MembersProvider>
-      <InviteeEmailProvider>
-        <DashboardsProvider>
-          <UserProvider>
-            <div className={S.container}>
-              <SideMenu />
-              <div className={S.rightSideContainer}>
-                <nav>
-                  <DashboardHeader pathname={pathname} />
-                </nav>
-                <main className={S.children}>{children}</main>
-              </div>
-            </div>
-          </UserProvider>
-        </DashboardsProvider>
-      </InviteeEmailProvider>
-    </MembersProvider>
+    <AppProvider
+      contexts={[
+        MembersProvider,
+        InviteeEmailProvider,
+        DashboardsProvider,
+        UserProvider,
+      ]}
+    >
+      <div className={S.container}>
+        <SideMenu />
+        <div className={S.rightSideContainer}>
+          <nav>
+            <DashboardHeader pathname={pathname} />
+          </nav>
+          <main className={S.children}>{children}</main>
+        </div>
+      </div>
+    </AppProvider>
   )
 }
 
